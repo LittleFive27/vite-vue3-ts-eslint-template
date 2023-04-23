dabdbcee0798b5de98816dc1473710383baaf7bb
wangling <wl_carol27@163.com>
2023-04-23 11:41:29 +0800

    feat: sass具体样式

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import './style.css'
 import App from './App.vue'
 import store from '@/store'
 
+import '@/assets/styles/reset.scss'
+import '@/assets/styles/scrollbar.scss'
+import '@/assets/styles/common.scss'
+
 const app = createApp(App)
 
 app.use(store)
