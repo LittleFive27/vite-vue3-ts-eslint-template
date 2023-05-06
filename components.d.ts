fe1c19c82a9aac996f6c53a3e1b02b9a6e31b66a
wangling <wl_carol27@163.com>
2023-05-06 16:31:19 +0800

    feat: gsap

--- components.d.ts
+++ components.d.ts
@@ -13,9 +13,5 @@ declare module '@vue/runtime-core' {
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
-    VanButton: typeof import('vant/es')['Button']
-    VanCell: typeof import('vant/es')['Cell']
-    VanCellGroup: typeof import('vant/es')['CellGroup']
-    VanPopup: typeof import('vant/es')['Popup']
   }
 }
